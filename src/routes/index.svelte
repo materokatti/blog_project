ae6d34414b8212c2a9f26840025f6c90223117fa
mateo <materokatti@gmail.com>
2022-01-06 23:10:46 +0900

    First commit with Front door

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<div class="min-h-screen flex flex-col iten-center justify-center">
+	<h1
+		class="text-gray-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white"
+	>
+		Hi my name is Doyun
+	</h1>
+	<p class="mt-2 text-sm sm:text-lg text-gray-600 text-center max-w-3xl mx-auto dark:text-gray-400">
+		Web Developer Who love Design
+	</p>
+</div>
